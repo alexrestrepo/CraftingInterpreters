9683b12a4848033afd3dab982da02b1b93298d90
Alex Restrepo <alexrestrepo@mac.com>
2019-06-12 22:14:49 -0700

    Initial Commit

---
+++ Clox/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Clox
+//
+//  Created by Alex Restrepo on 6/12/19.
+//  Copyright © 2019 arg. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
