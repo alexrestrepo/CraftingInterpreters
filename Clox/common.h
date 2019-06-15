66d86271e8e76baebd889a47610a97073a1ea5a1
Alex Restrepo <alexrestrepo@mac.com>
2019-06-14 19:00:53 -0700

    ch 17

--- Clox/common.h
+++ Clox/common.h
@@ -13,6 +13,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
 
 #endif /* common_h */
--- Clox/compiler.h
+++ Clox/compiler.h
@@ -9,6 +9,8 @@
 #ifndef compiler_h
 #define compiler_h
 
-void compile(const char *source);
+#include "vm.h"
+
+bool compile(const char *source, Chunk *chunk);
 
 #endif /* compiler_h */
