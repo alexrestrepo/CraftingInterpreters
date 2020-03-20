28bf7598156329b6d1e4807e328cbaf38f106c74
Alex Restrepo <alexrestrepo@mac.com>
2020-03-20 14:59:01 -0700

    Ch 28 Superclasses

--- Clox/compiler.c
+++ Clox/compiler.c
@@ -81,6 +81,7 @@ typedef struct Compiler {
 typedef struct ClassCompiler {
     struct ClassCompiler *enclosing;
     Token name;
+    bool hasSuperclass;
 } ClassCompiler;
 
 Parser parser;
@@ -330,7 +331,7 @@ static void dot(bool canAssign) {
         uint8_t argCount = argumentList();
         emitBytes(OP_INVOKE, name);
         emitByte(argCount);
-        
+
     } else {
         emitBytes(OP_GET_PROPERTY, name);
     }
@@ -412,6 +413,38 @@ static void variable(bool canAssign) {
     namedVariable(parser.previous, canAssign);
 }
 
+static Token syntheticToken(const char* text) {
+    Token token;
+    token.start = text;
+    token.length = (int)strlen(text);
+    return token;
+}
+
+static void super_(bool canAssign) {
+    if (currentClass == NULL) {
+        error("Cannot use 'super' outside of a class.");
+
+    } else if (!currentClass->hasSuperclass) {
+        error("Cannot use 'super' in a class with no superclass.");
+    }
+
+    consume(TOKEN_DOT, "Expect '.' after 'super'.");
+    consume(TOKEN_IDENTIFIER, "Expect superclass method name.");
+    uint8_t name = identifierConstant(&parser.previous);
+
+    namedVariable(syntheticToken("this"), false);
+    if (match(TOKEN_LEFT_PAREN)) {
+        uint8_t argCount = argumentList();
+        namedVariable(syntheticToken("super"), false);
+        emitBytes(OP_SUPER_INVOKE, name);
+        emitByte(argCount);
+        
+    } else {
+        namedVariable(syntheticToken("super"), false);
+        emitBytes(OP_GET_SUPER, name);
+    }
+}
+
 static void this_(bool canAssign) {
     if (currentClass == NULL) {
         error("Cannot use 'this' outside of a class.");
@@ -469,8 +502,8 @@ ParseRule rules[] = {
     { NULL,     or_,     PREC_OR },         // TOKEN_OR
     { NULL,     NULL,    PREC_NONE },       // TOKEN_PRINT
     { NULL,     NULL,    PREC_NONE },       // TOKEN_RETURN
-    { NULL,     NULL,    PREC_NONE },       // TOKEN_SUPER
-    { this_,     NULL,   PREC_NONE },       // TOKEN_THIS
+    { super_,   NULL,    PREC_NONE },       // TOKEN_SUPER
+    { this_,    NULL,    PREC_NONE },       // TOKEN_THIS
     { literal,  NULL,    PREC_NONE },       // TOKEN_TRUE
     { NULL,     NULL,    PREC_NONE },       // TOKEN_VAR
     { NULL,     NULL,    PREC_NONE },       // TOKEN_WHILE
@@ -724,8 +757,27 @@ static void classDeclaration() {
 
     ClassCompiler classCompiler;
     classCompiler.name = parser.previous;
+    classCompiler.hasSuperclass = false;
     classCompiler.enclosing = currentClass;
     currentClass = &classCompiler;
+
+    if (match(TOKEN_LESS)) {
+        consume(TOKEN_IDENTIFIER, "Expect superclass name.");
+        variable(false);
+
+        if (identifiersEqual(&className, &parser.previous)) {
+            error("A class cannot inherit from itself.");
+        }
+
+        beginScope();
+        addLocal(syntheticToken("super"));
+        defineVariable(0);
+
+        namedVariable(className, false);
+        emitByte(OP_INHERIT);
+
+        classCompiler.hasSuperclass = true;
+    }
 
     namedVariable(className, false);
     consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
@@ -734,6 +786,10 @@ static void classDeclaration() {
     }
     consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
     emitByte(OP_POP);
+
+    if (classCompiler.hasSuperclass) {
+        endScope();
+    }
 
     currentClass = currentClass->enclosing;
 }
@@ -849,7 +905,7 @@ static void returnStatement() {
         if (current->type == TYPE_INITIALIZER) {
             error("Cannot return a value from an initializer.");
         }
-        
+
         expression();
         consume(TOKEN_SEMICOLON, "Expect ';' after return value.");
         emitByte(OP_RETURN);
